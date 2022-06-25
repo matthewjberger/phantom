b3f6a56ef7c26348ea1dd0207e2be310df688966
Matthew J. Berger <matthewberger@nevada.unr.edu>
2022-06-25 02:09:43 -0600

    Use actual window scale factor

--- crates/phantom_render/src/renderer.rs
+++ crates/phantom_render/src/renderer.rs
@@ -12,16 +12,18 @@ pub enum Backend {
 pub trait Renderer {
     fn resize(&mut self, dimensions: [u32; 2]);
     fn render(&mut self, gui_context: &Context, paint_jobs: Vec<ClippedMesh>) -> Result<()>;
+    fn set_scale_factor(&mut self, scale_factor: f64);
 }
 
 pub fn create_render_backend(
     backend: &Backend,
     window_handle: &impl HasRawWindowHandle,
     dimensions: &[u32; 2],
+    scale_factor: f64,
 ) -> Result<Box<dyn Renderer>> {
     match backend {
         Backend::Wgpu => {
-            let backend = WgpuRenderer::new(window_handle, dimensions)?;
+            let backend = WgpuRenderer::new(window_handle, dimensions, scale_factor)?;
             Ok(Box::new(backend) as Box<dyn Renderer>)
         }
     }
